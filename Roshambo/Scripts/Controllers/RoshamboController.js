71ea107100063647bafac67e352da6f4e0c38620
MagL33To <stefan.benjamin@gmail.com>
2015-11-03 16:04:32 +0000

    controllers folder in scripts was untracked for some reason

---
+++ Roshambo/Scripts/Controllers/RoshamboController.js
@@ -0,0 +1,28 @@
+﻿app.controller("RoshamboController", ["$scope", "$http", function($scope, $http) {
+    $scope.humanPlayer = true;
+    $scope.selectedMove = 1;
+
+    $scope.getResult = function (move) {
+        if (!$scope.humanPlayer) {
+            $scope.selectedMove = Math.floor(Math.random() * 3);
+        } else {
+            $scope.selectedMove = move;
+        }
+        $http({ method: "POST", url: "/Home/PlayMove?playerMove=" + $scope.selectedMove + "&isPlayerHuman=" + $scope.humanPlayer })
+        .success(function(data) {
+            alert(data.ResultText);
+            });
+    };
+
+    $scope.setGameUp = function(isHumanGame) {
+        if (!isHumanGame) {
+            $scope.humanPlayer = false;
+            $("#mainGame").hide();
+            $("#cpuGame").show();
+        } else {
+            $scope.humanPlayer = true;
+            $("#mainGame").hide();
+            $("#humanGame").show();
+        }
+    };
+}]);
